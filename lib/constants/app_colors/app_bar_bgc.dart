08a562ba12e1d209eae8726f68e6d046ed85e0d1
Saparbek81 <saparbekmamatov81@gmail.com>
2024-12-20 23:45:12 +0600

    done

--- lib/constants/app_colors/app_bar_bgc.dart
+++ lib/constants/app_colors/app_bar_bgc.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-class AppBarBgc {
+class AppColors {
   static const Color appBarBgc = Color(0xfffe5722);
   static const Color scaffoldBgc = Color(0xffffffff);
 }
+
+const appbarcolor = AppColors.appBarBgc;
+const scaffoldColor = AppColors.scaffoldBgc;
--- lib/features/presentation/my_home_page.dart
+++ lib/features/presentation/my_home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:sabak_17_news_app_ui/constants/app_colors/app_bar_bgc.dart';
+import 'package:sabak_17_news_app_ui/constants/app_colors/icons_color.dart';
+import 'package:sabak_17_news_app_ui/constants/text_styles/title_color.dart';
+import 'package:sabak_17_news_app_ui/features/data/oop.dart';
+import 'package:sabak_17_news_app_ui/widgets/news_card.dart';
+import 'package:sabak_17_news_app_ui/widgets/search_widget.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -11,11 +15,38 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppBarBgc.appBarBgc,
-        title: const Text("News Agregator"),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: orangColor,
+        appBar: myAppBar(),
+        body: ListView.builder(
+            itemCount: newsList.length,
+            itemBuilder: (context, index) {
+              return NewCard(
+                index: index,
+                newsList: newsList,
+              );
+
+              Text(index.toString());
+            }),
+        floatingActionButton: const SearchWidget(),
       ),
+    );
+  }
+
+  AppBar myAppBar() {
+    return AppBar(
+      backgroundColor: orangColor,
+      title: const Text(
+        "News Agregator",
+        style: titleStyle,
+      ),
+      actions: const [
+        Icon(
+          Icons.more_vert,
+          color: searchColor,
+        ),
+      ],
     );
   }
 }
